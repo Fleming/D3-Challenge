02eca419b4aa7f918d5852b437cab3c4980b626f
Fleming <47439078+Fleming@users.noreply.github.com>
2021-05-19 15:28:13 -0400

    Update app.js

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -342,4 +342,63 @@ function updateToolTip(chosenXAxis, chosenYAxis, circlesGroup) {
             }
         });
 
-        
+        yLabelsGroup.selectAll("text")
+        .on("click", function () {
+            // get value of selection
+            var yValue = d3.select(this).attr("value");
+            if (yValue !== chosenYAxis) {
+
+                // replaces chosenXAxis with value
+                chosenYAxis = yValue;
+
+                // functions here found above csv import
+                // updates x scale for new data
+                yLinearScale = yScale(censusData, chosenYAxis);
+
+                // updates x axis with transition
+                yAxis = renderYAxes(yLinearScale, yAxis);
+
+                // updates circles and circle text with new y values
+                circlesGroup = renderYCircles(circlesGroup, yLinearScale, chosenYAxis);
+                textCircles = renderYCircleText(textCircles, yLinearScale, chosenYAxis);
+
+                // updates tooltips with new info
+                circlesGroup = updateToolTip(chosenXAxis, chosenYAxis, circlesGroup);
+
+                // changes classes to change bold text
+                if (chosenYAxis === "income") {
+                    incomeLabel
+                        .classed("active", true)
+                        .classed("inactive", false);
+                    ageLabel
+                        .classed("active", false)
+                        .classed("inactive", true);
+                    povertyLabel
+                        .classed("active", false)
+                        .classed("inactive", true);
+                }
+                else if (chosenYAxis === "poverty") {
+                    incomeLabel
+                        .classed("active", false)
+                        .classed("inactive", true);
+                    ageLabel
+                        .classed("active", false)
+                        .classed("inactive", true);
+                    povertyLabel
+                        .classed("active", true)
+                        .classed("inactive", false);
+                }
+                else {
+                    incomeLabel
+                        .classed("active", false)
+                        .classed("inactive", true);
+                    ageLabel
+                        .classed("active", true)
+                        .classed("inactive", false);
+                    povertyLabel
+                        .classed("active", false)
+                        .classed("inactive", true);
+                }
+            }
+        });
+})()
